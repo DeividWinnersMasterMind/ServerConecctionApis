0dd24ad6b66832a1a23058efd9c63109830fbe9f
Alejandro Rojas Martinez <deivid@winnersmastermind.com>
2024-04-29 13:56:29 -0500

    update proyect

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+// Analizar el cuerpo de la solicitud como JSON
+app.use(express.json());
+
+// Manejador para la ruta '/registro'
+app.post('/registro', (req, res) => {
+    // Recibir y procesar los datos enviados desde el cliente
+    let data = req.body;
+
+    console.log(JSON.stringify(data))
+
+    fetch('https://p100-ld.irev.com/api/affiliates/v2/leads', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': '7voitrmdnhitdaftt9j5g84beoyg9axf3'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            /*  console.log('Respuesta del servidor:', data); */
+            console.log(data)
+        })
+        .catch(error => {
+            console.error('Error al enviar la solicitud:', error);
+            res.status(500).send('Error al enviar la solicitud');
+        });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
+
