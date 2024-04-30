a1f1bb91cbb8f41fb9c3f1c759179ceafc2d2c1f
Alejandro Rojas Martinez <deivid@winnersmastermind.com>
2024-04-29 22:09:38 -0500

    update server

--- server.js
+++ server.js
@@ -1,53 +1,40 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
-
 
 // Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 // Analizar el cuerpo de la solicitud como JSON
 app.use(express.json());
 
-import('node-fetch').then((fetch) => {
-    const app = express();
+// Manejador para la ruta '/registro'
+app.post('/registro', (req, res) => {
+    // Recibir y procesar los datos enviados desde el cliente
+    let data = req.body;
 
-    // Resto de tu código ...
+    console.log(JSON.stringify(data));
 
-    // Manejador para la ruta '/registro'
-    app.post('/registro', async (req, res) => {
-        try {
-            // Recibir y procesar los datos enviados desde el cliente
-            let data = req.body;
-
-            console.log(JSON.stringify(data));
-
-            const response = await fetch.default('https://p100-ld.irev.com/api/affiliates/v2/leads', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': '7voitrmdnhitdaftt9j5g84beoyg9axf3'
-                },
-                body: JSON.stringify(data)
-            });
-
-            const responseData = await response.json();
-
-            console.log(responseData);
-
-            res.json(responseData);
-        } catch (error) {
-            console.error('Error al enviar la solicitud:', error);
-            res.status(500).send('Error al enviar la solicitud');
+    axios.post('https://p100-ld.irev.com/api/affiliates/v2/leads', data, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': '7voitrmdnhitdaftt9j5g84beoyg9axf3'
         }
+    })
+    .then(response => {
+        console.log(response.data);
+        res.json(response.data);
+    })
+    .catch(error => {
+        console.error('Error al enviar la solicitud:', error);
+        res.status(500).send('Error al enviar la solicitud');
     });
-
-    // Resto de tu código ...
 });
-const PORT = 3000
 
-app.listen(3000, () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
-
